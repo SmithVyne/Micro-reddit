7b82bed5e03e5d031ca7de0647c1f12b075aa029
Paul Omondi <paulo.techie@gmail.com>
2020-07-08 23:34:02 +0300

    update comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
     validates   :email, presence: true,
                 length: {maximum: 30}
     validates   :password, presence: true,
--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_07_08_181706) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
