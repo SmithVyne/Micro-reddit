7fd2c5139c08bfe72f677a01e717d1d34a1ad3ae
SmithVyne <smithnkereuwem2@gmail.com>
2020-07-09 00:42:25 +0300

    Complete the comments

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,9 @@ ActiveRecord::Schema.define(version: 2020_07_08_181706) do
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,5 +39,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_181706) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
