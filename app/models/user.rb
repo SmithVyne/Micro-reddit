90150c82f78446c5c78a5ecf63ecb6163d0e72a6
Paul Omondi <paulo.techie@gmail.com>
2020-07-09 18:37:20 +0300

    remove unnecessary spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :comments
   validates :email, presence: true,
                     length: { maximum: 30 }
-  validates :password,  presence: true,
-                        length: { minimum: 4 }
+  validates :password, presence: true,
+                       length: { minimum: 4 }
 end
