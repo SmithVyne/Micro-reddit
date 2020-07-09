642bde160f08baa8b95ac8cd8b9fdb9c5c40abb3
Paul Omondi <paulo.techie@gmail.com>
2020-07-09 18:34:33 +0300

    align  hash keys on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   validates :email, presence: true,
-                      length: { maximum: 30 }
-  validates :password, presence: true,
-                         length: { minimum: 4 }
+                    length: { maximum: 30 }
+  validates :password,  presence: true,
+                        length: { minimum: 4 }
 end
