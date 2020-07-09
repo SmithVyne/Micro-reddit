f5b9c540645a57429a89c7bd178417501eef642f
Paul Omondi <paulo.techie@gmail.com>
2020-07-09 18:31:10 +0300

    linter fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
-  validates   :email, presence: true,
+  validates :email, presence: true,
                       length: { maximum: 30 }
-  validates   :password, presence: true,
+  validates :password, presence: true,
                          length: { minimum: 4 }
 end
