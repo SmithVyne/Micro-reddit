3191ccb5a967407f5b7710f1b48d20d12b841f9c
Paul Omondi <paulo.techie@gmail.com>
2020-07-09 18:14:09 +0300

    linter corrections

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many  :comments
   validates :title, presence: true,
-            length: {maximum: 50}
+                    length: { maximum: 50 }
   validates :body, presence: true
 end
